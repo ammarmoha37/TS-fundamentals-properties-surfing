b9e7ddca635a9b7f9718362495a304dcbbd79993
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-02-15 12:59:56 +0300

    display total reviews

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,28 @@
 "use strict";
 const returningUserDisplay = document.querySelector('#returning-user');
 const userNameDisplay = document.querySelector('#user');
+const reviewTotalDisplay = document.querySelector('#reviews');
 const you = {
     userName: 'Bobby',
     isReturning: true,
 };
+const reviews = [
+    {
+        name: 'Tom',
+        stars: 5,
+        loyalityUser: true,
+    },
+    {
+        name: 'Ally',
+        stars: 3,
+        loyalityUser: false,
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyalityUser: true,
+    }
+];
 function displayUser(isReturning, userName) {
     if (returningUserDisplay) {
         if (isReturning) {
@@ -16,3 +34,8 @@ function displayUser(isReturning, userName) {
     }
 }
 displayUser(you.isReturning, you.userName);
+const showTotalReviews = (value, reviewer, loyality) => {
+    const showStar = loyality ? '🌟' : '';
+    reviewTotalDisplay.innerHTML = value + ' reviews' + '| last review by ' + reviewer + ' ' + showStar;
+};
+showTotalReviews(reviews[0].stars, reviews[0].name, reviews[0].loyalityUser);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-const returningUserDisplay  = document.querySelector('#returning-user');
-const userNameDisplay = document.querySelector('#user');
+const returningUserDisplay  = document.querySelector('#returning-user')!;
+const userNameDisplay = document.querySelector('#user')!;
+const reviewTotalDisplay = document.querySelector('#reviews')!;
 
 const you : {
   userName: string;
@@ -8,6 +9,28 @@ const you : {
   userName: 'Bobby',
   isReturning: true,
 }
+
+const reviews: {
+  name: string;
+  stars: number;
+  loyalityUser: boolean;
+}[] = [
+  {
+    name: 'Tom',
+    stars: 5,
+    loyalityUser: true,
+  },
+  {
+    name: 'Ally',
+    stars: 3,
+    loyalityUser: false,
+  },
+  {
+    name: 'Omar',
+    stars: 4,
+    loyalityUser: true,
+  }
+]
 
 function displayUser(isReturning : boolean, userName : string) {
   if (returningUserDisplay) {
@@ -21,3 +44,10 @@ function displayUser(isReturning : boolean, userName : string) {
 } 
 
 displayUser(you.isReturning, you.userName);
+
+const showTotalReviews = (value: number, reviewer: string, loyality: boolean) => {
+  const showStar = loyality ? '🌟' : '';
+  reviewTotalDisplay.innerHTML = value + ' reviews' + '| last review by ' + reviewer + ' ' + showStar; 
+}
+
+showTotalReviews(reviews[0].stars, reviews[0].name, reviews[0].loyalityUser);
