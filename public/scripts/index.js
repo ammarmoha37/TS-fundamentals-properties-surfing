96b344b5ce6955a070c0d6782fd44ea1f9f2eb49
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-02-15 12:33:39 +0300

    user welcoming

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('hello');
+const returningUserDisplay = document.querySelector('#returning-user');
+const userNameDisplay = document.querySelector('#user');
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+};
+function displayUser(isReturning, userName) {
+    if (returningUserDisplay) {
+        if (isReturning) {
+            returningUserDisplay.innerHTML = 'back';
+        }
+    }
+    if (userNameDisplay) {
+        userNameDisplay.innerHTML = userName;
+    }
+}
+displayUser(you.isReturning, you.userName);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello');
+const returningUserDisplay  = document.querySelector('#returning-user');
+const userNameDisplay = document.querySelector('#user');
+
+const you : {
+  userName: string;
+  isReturning: boolean;
+} = {
+  userName: 'Bobby',
+  isReturning: true,
+}
+
+function displayUser(isReturning : boolean, userName : string) {
+  if (returningUserDisplay) {
+    if (isReturning) {
+      returningUserDisplay.innerHTML = 'back';
+    }
+  }
+  if (userNameDisplay) {
+    userNameDisplay.innerHTML = userName;
+  }
+} 
+
+displayUser(you.isReturning, you.userName);
