e6f2c32cdc081c54ee75c7fea8a3cd96dd6a8b98
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-02-16 15:12:07 +0300

    adding properties

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,7 @@ const myMainProperty = document.querySelector(".main-image");
 const reviewsButton = document.querySelector('button');
 const reviewsContainer = document.querySelector('.reviews');
 const container = document.querySelector('.container');
+const propertiesContainer = document.querySelector('.properties');
 const you = {
     userName: 'Bobby',
     isReturning: true,
@@ -58,3 +59,69 @@ const displayReviews = (reviews) => {
     }
 };
 reviewsButton.addEventListener('click', () => displayReviews(reviews));
+const properties = [
+    {
+        image: '../public/imgs/colombia-property.jpg',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: [+34589027906, 'marywinklelinkle@gmail.com'],
+        isAvailable: true,
+    },
+    {
+        image: '../public/imgs/poland-property.jpg',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: [+90390245784, 'garyadvices@hotmail.com'],
+        isAvailable: false,
+    },
+    {
+        image: '../public/imgs/london-property.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom',
+        },
+        contact: [+2934589732, 'andyluger@aol.com'],
+        isAvailable: true,
+    },
+    {
+        image: '../public/imgs/malaysian-hotel.jpeg',
+        title: 'Malia Hotel',
+        price: 35,
+        location: {
+            firstLine: 'Room 4',
+            city: 'Malia',
+            code: 45334,
+            country: 'Malaysia'
+        },
+        contact: [+60349822083, 'lolee34@gmail.com'],
+        isAvailable: false
+    }
+];
+//Adding properties
+if (propertiesContainer) {
+    for (let i = 0; i < properties.length; i++) {
+        const cardProperty = document.createElement('div');
+        cardProperty.classList.add('card-property');
+        cardProperty.innerHTML = properties[i].title;
+        const image = document.createElement('img');
+        image.setAttribute('src', properties[i].image);
+        cardProperty.appendChild(image);
+        propertiesContainer.appendChild(cardProperty);
+    }
+}
